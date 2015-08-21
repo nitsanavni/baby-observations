6931f4a09e9bc56712f5ffba3cefeea7acc6d60f
Nitsan Avni <nitsanav@gmail.com>
2015-08-21 15:26:22 +0300

    basic export session

--- app/src/main/java/com/meirco/babyobservations/ui/SessionFragment.java
+++ app/src/main/java/com/meirco/babyobservations/ui/SessionFragment.java
@@ -2,9 +2,11 @@ package com.meirco.babyobservations.ui;
 
 import android.app.Fragment;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.method.CharacterPickerDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,11 +14,14 @@ import android.view.ViewGroup;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.meirco.babyobservations.R;
 import com.meirco.babyobservations.db.DbHelper;
 import com.meirco.babyobservations.di.Injector;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 import javax.inject.Inject;
@@ -65,7 +70,40 @@ public class SessionFragment extends Fragment {
         list.setAdapter(new Adapter(getActivity(), mDbHelper.get().getSessionEntries(mSessionId)));
         TextView title = (TextView) content.findViewById(R.id.title);
         title.setText(getString(R.string.session_title, mSessionId));
+        content.findViewById(R.id.export_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                exportSessionText();
+            }
+        });
         return content;
+    }
+
+    private void exportSessionText() {
+        Cursor cursor = mDbHelper.get().getSessionEntries(mSessionId);
+        if (cursor == null || !cursor.moveToFirst()) {
+            Toast.makeText(getActivity(), R.string.export_session_no_entries_msg, Toast.LENGTH_SHORT).show();
+            return;
+        }
+        StringBuilder sb = new StringBuilder();
+        Calendar calendar = Calendar.getInstance();
+        DateFormat dateFormat = SimpleDateFormat.getTimeInstance();
+        while (!cursor.isAfterLast()) {
+            long entryCreated = DbHelper.getEntryCreated(cursor);
+            calendar.setTimeInMillis(entryCreated);
+            dateFormat.setCalendar(calendar);
+            String format = dateFormat.format(calendar.getTime());
+            String entryText = mDbHelper.get().getSeenText(DbHelper.getEntryText(cursor));
+            sb.append(format)
+                    .append("\t")
+                    .append(entryText)
+                    .append("\n");
+            cursor.moveToNext();
+        }
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_TEXT, sb.toString());
+        intent.setType("text/plain");
+        startActivity(intent);
     }
 
     private static class Adapter extends CursorAdapter {
